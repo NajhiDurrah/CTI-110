c754498b7fef6c959e09c97b2593c1cb98286143
NajhiDurrah <43180625+NajhiDurrah@users.noreply.github.com>
2018-09-11 12:16:42 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_NajhiDurrah.py
@@ -0,0 +1,17 @@
+#Sales prediiction
+#September 11 2018
+#CTI-110 P2T1 - Sales Prediction
+#Najhi Durrah
+#
+
+#Ask the user to enter the projected amount of total sales.
+projected_Total_Sales = float(input('What are the projected total sales? ' ))
+
+#Calcualte the Annual profit
+annual_profit = projected_Total_Sales * 0.23
+
+#Print the annual profit
+print('The annual profit is', annual_profit)
+
+
+
